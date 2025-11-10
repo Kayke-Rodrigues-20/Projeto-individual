1dde00834befc24dbea66564d2a31bb6119fc8e5
Kayke-Rodrigues-20 <kayke.silva@sptech.school>
2025-11-09 23:34:08 -0300

    Primeiro commit

---
+++ redemption/red.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE RED;
+USE RED;
+
+CREATE TABLE empresa (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	plataforma VARCHAR(50),
+	codigoPlataforma VARCHAR(50)
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+insert into empresa (plataforma, codigoPlataforma) values ('Playstation', 'play-ative');
+insert into empresa (plataforma, codigoPlataforma) values ('Xbox', 'xbox-ative');
+insert into empresa (plataforma, codigoPlataforma) values ('Pc', 'pc-ative');
+insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 3);
+
+delete from usuario
+where id >= 7;
+
+select * from usuario;
+select * from empresa;

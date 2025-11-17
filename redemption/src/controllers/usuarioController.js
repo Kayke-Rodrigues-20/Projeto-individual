3fc3158e7d452019fd322be0a3d4c788c00867e7
Kayke-Rodrigues-20 <kayke.silva@sptech.school>
2025-11-16 23:26:00 -0300

    estilização da tela d inicial

--- redemption/src/controllers/usuarioController.js
+++ redemption/src/controllers/usuarioController.js
@@ -33,7 +33,7 @@ function autenticar(req, res) {
                                 } else {
                                     res.status(204).json({ aquarios: [] });
                                 }
-                            })
+                            }) 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
